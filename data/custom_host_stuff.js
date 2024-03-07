5b3837a77a06d60c1ff608048258edc5a8733ff3
- <idlesauce@privatemail.com>
2024-03-07 10:40:13 +0100

    john-tornblom-elfldr loader, update john-tornblom-elfldr, add ps5-payload-shsrv & cleanup

--- data/custom_host_stuff.js
+++ data/custom_host_stuff.js
@@ -369,7 +369,7 @@ function create_payload_buttons() {
 
 }
 
-function showToast(message) {
+function showToast(message, timeout = 2000) {
     const toastContainer = document.getElementById('toast-container');
     const toast = document.createElement('div');
     toast.className = 'toast';
@@ -387,5 +387,5 @@ function showToast(message) {
         toast.addEventListener('transitionend', () => {
             toast.remove();
         });
-    }, 2000);
+    }, timeout);
 }
